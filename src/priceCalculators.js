d680f471c77d55e9d33177b05dee3b0ba4a3ba80
David Franco <david@mejuri.com>
2020-12-06 11:18:00 -0300

    Add MegaCoverageCalculator + CarInsumer as Context

---
+++ src/priceCalculators.js
@@ -0,0 +1,19 @@
+class Calculator {
+  constructor(product){
+    this.product = product
+  }
+}
+
+class MegaCoverageCalculator {
+  constructor(product) {
+    this.product = product
+  }
+
+  updateAmount() {}
+
+  updateSellIn() {}
+}
+
+module.exports = {
+  MegaCoverageCalculator
+}
